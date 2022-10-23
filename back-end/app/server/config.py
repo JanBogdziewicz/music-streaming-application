3bcc6f319273988727e31ce203688423b163a6b2
JakGorn <jakgor00@gmail.com>
2022-10-23 15:35:39 +0200

    Running mongo with user authentication

--- back-end/app/server/config.py
+++ back-end/app/server/config.py
@@ -1,5 +1,5 @@
 import motor.motor_asyncio
 
-MONGO_DETAILS = "mongodb:27017"
+MONGO_DETAILS = "mongodb://music:music@mongodb:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 database = client.music
