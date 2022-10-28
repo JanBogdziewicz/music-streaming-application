dd719337c4ed3fcd310f629b9d16fe036656a2f8
jan_bogdziewicz <jan.bogdziewicz@comarch.pl>
2022-10-28 16:49:33 +0200

    playlist minor corrections

--- front-end/src/app/services/playlist.service.ts
+++ front-end/src/app/services/playlist.service.ts
@@ -2,8 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { map, tap } from 'rxjs/operators';
-import { Album } from 'src/app/database-entities/album';
-import { Artist } from 'src/app/database-entities/artist';
 import { MongoResponse } from 'src/app/database-entities/mongo_response';
 import { Song } from 'src/app/database-entities/song';
 import { environment } from 'src/environments/environment';
