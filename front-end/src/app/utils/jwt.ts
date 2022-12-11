5e461b2d62658bb4a1451f6702da449c21e26baf
jan_bogdziewicz <jan.bogdziewicz@comarch.pl>
2022-12-11 10:26:58 +0100

    fixed bug with incognito mode

--- front-end/src/app/utils/jwt.ts
+++ front-end/src/app/utils/jwt.ts
@@ -1,6 +1,11 @@
 import jwtDecode from "jwt-decode";
 
 export function getUsernameFromToken(): string {
-  const decoded = jwtDecode(localStorage.getItem('access_token') as string) as any;
-  return decoded.sub;
+  let decoded;
+  try {
+    decoded = jwtDecode(localStorage.getItem('access_token') as string) as any;
+    return decoded.sub;
+  } catch (error) {
+    return "";
+  }
 }
